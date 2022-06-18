3e848f89006bb64e88e9a4532ab8730059cc8e98
ZERDICORP <nikolaianikin2002@gmail.com>
2022-06-18 11:57:43 +0300

    init

---
+++ src/main/java/com/yandex_backend_school/mega_market/MegaMarketApplication.java
@@ -0,0 +1,11 @@
+package com.yandex_backend_school.mega_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaMarketApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MegaMarketApplication.class, args);
+  }
+}
---
+++ src/test/java/com/yandex_backend_school/mega_market/MegaMarketApplicationTests.java
@@ -0,0 +1,11 @@
+package com.yandex_backend_school.mega_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaMarketApplicationTests {
+  @Test
+  void contextLoads() {
+  }
+}
